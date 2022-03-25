52022d5d8cb82b85695ab924d43773cd8066542d
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-03-25 12:01:13 +0300

    init mac

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn
 import macros
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def get_weights_initializer(std_hp=1.0,Xaviar_init=False):
     def weights_init(m):
